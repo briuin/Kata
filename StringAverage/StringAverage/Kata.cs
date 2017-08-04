414b083735b6f9c2b7482717e1b038f8dd2b5290
Edward Lim Chun Hoa <edward.lim61@titansoft.com.sg>
2017-08-04 13:52:48 +0800

    add stringaverage method and test

---
+++ StringAverage/StringAverage/Kata.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAverage
+{
+    public class Kata
+    {
+        public static string StringAverage(string inputString)
+        {
+            var numberStrings = new Dictionary<string, int>()
+            {
+                {"zero",0},
+                {"one",1},
+                {"two",2},
+                {"three",3},
+                {"four",4},
+                {"five",5},
+                {"six",6},
+                {"seven",7},
+                {"eight",8},
+                {"nine",9}
+            };
+
+            if (string.IsNullOrEmpty(inputString.Trim()))
+                return "n/a";
+
+            var numbers = new List<int>();
+            foreach (var value in inputString.Split(' '))
+                numbers.Add(numberStrings[value]);
+
+            return numberStrings.FirstOrDefault(x => x.Value == (int)(numbers.Average())).Key;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ StringAverage/StringAverageTest/KataTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringAverage;
+
+namespace StringAverageTest
+{
+    [TestClass]
+    public class KataTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("four", Kata.StringAverage("zero nine five two"));
+            Assert.AreEqual("three", Kata.StringAverage("four six two three"));
+            Assert.AreEqual("three", Kata.StringAverage("one two three four five"));
+            Assert.AreEqual("four", Kata.StringAverage("five four"));
+            Assert.AreEqual("zero", Kata.StringAverage("zero zero zero zero zero"));
+            Assert.AreEqual("two", Kata.StringAverage("one one eight one"));
+            Assert.AreEqual("n/a", Kata.StringAverage(""));
+        }
+    }
+}
