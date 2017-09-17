0b4018ba8770aef75439cb939347933a2805cadf
Edward Lim Chun Hoa <edward.lim61@titansoft.com.sg>
2017-09-18 02:19:27 +0800

    add NextBiggerNumber solution

---
+++ NextBiggerNumber/NextBiggerNumber/Kata.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextBiggerNumber
+{
+    public class Kata
+    {
+        private static void Swap(List<char> list, int index1, int index2)
+        {
+            var temp = list[index1];
+            list[index1] = list[index2];
+            list[index2] = temp;
+        }
+
+        private static int ConvertCharListToInt(List<char> list)
+        {
+            var result = 0;
+            list.ForEach(n => result = result * 10 + (int)Char.GetNumericValue(n));
+
+            return result;
+        }
+
+        public static void FindAndSwapLowerNumber(List<char> list)
+        {
+            for (int i = list.Count - 1; i >= 1; i--)
+                for (int j = i - 1; j >= 0; j--)
+                    if (list[i] > list[j])
+                    {
+                        Swap(list, i, j);
+                        return;
+                    }
+        }
+
+        public static int NextBiggerNumber(uint number)
+        {
+            var charListNumbers = number.ToString().ToList();
+            FindAndSwapLowerNumber(charListNumbers);
+            var result = ConvertCharListToInt(charListNumbers);
+            return result == number ? -1 : result;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ NextBiggerNumber/NextBiggerNumberTest/KataTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NextBiggerNumber;
+
+namespace NextBiggerNumberTest
+{
+    [TestClass]
+    public class KataTest
+    {
+        [TestMethod]
+        public void Test_Input_12_Will_Output_21()
+        {
+            Assert.AreEqual(21, Kata.NextBiggerNumber(12));
+        }
+
+        [TestMethod]
+        public void Test_Input_513_Will_Output_531()
+        {
+            Assert.AreEqual(531, Kata.NextBiggerNumber(513));
+        }
+
+        [TestMethod]
+        public void Test_Input_2017_Will_Output_2071()
+        {
+            Assert.AreEqual(2071, Kata.NextBiggerNumber(2017));
+        }
+
+        [TestMethod]
+        public void Test_Input_9_Will_Output_negative1()
+        {
+            Assert.AreEqual(-1, Kata.NextBiggerNumber(9));
+        }
+
+        [TestMethod]
+        public void Test_Input_111_Will_Output_negative1()
+        {
+            Assert.AreEqual(-1, Kata.NextBiggerNumber(111));
+        }
+
+        [TestMethod]
+        public void Test_Input_531_Will_Output_negative1()
+        {
+            Assert.AreEqual(-1, Kata.NextBiggerNumber(531));
+        }
+
+        
+    }
+}
