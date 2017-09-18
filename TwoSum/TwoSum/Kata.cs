3011d702fb568fc41d1d6607d7db195cc0df24ad
Edward Lim Chun Hoa <edward.lim61@titansoft.com.sg>
2017-09-19 01:06:45 +0800

    add Two Sum

---
+++ TwoSum/TwoSum/Kata.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoSum
+{
+    
+    public class Kata
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            var indexNums = nums.Select((num, index) => new { num, index });
+            return indexNums.Where(x => indexNums.Where(y => x.index != y.index && x.num + y.num == target).Any()).Select(x => x.index).ToArray();
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ TwoSum/TwoSumTest/KataTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TwoSum;
+
+namespace TwoSumTest
+{
+    [TestClass]
+    public class KataTest
+    {
+        [TestMethod]
+        public void Test_Nums_2_7_11_5_And_Target_9_Should_Return_0_1()
+        {
+            CollectionAssert.AreEqual(new int[] { 0, 1 }, Kata.TwoSum(new int[] { 2, 7, 11, 5 }, 9));
+        }
+
+        [TestMethod]
+        public void Test_Nums_3_5_12_5_And_Target_10_Should_Return_1_3()
+        {
+            CollectionAssert.AreEqual(new int[] { 1, 3 }, Kata.TwoSum(new int[] { 3, 5, 12, 5 }, 10));
+        }
+    }
+}
