5697d854325ecd2bd4454750b584339c57a32649
Edward Lim Chun Hoa <edward.lim61@titansoft.com.sg>
2017-09-20 00:37:21 +0800

    add dead ants

---
+++ DeadAnts/DeadAnts/Kata.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DeadAnts
+{
+    public class Kata
+    {
+        public static int FindOutDeadAntsCount(string ants)
+        {
+            int antsAlive = Regex.Matches(ants, "ant").Cast<Match>().Count();
+            int antDeadHead = Regex.Matches(ants, "a").Cast<Match>().Count();
+            int antDeadBody = Regex.Matches(ants, "n").Cast<Match>().Count();
+            int antDeatFoot = Regex.Matches(ants, "t").Cast<Match>().Count();
+
+            return new List<int>{antDeadHead,antDeadBody,antDeatFoot}.Min() - antsAlive;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ DeadAnts/DeadAntsTest/KataTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DeadAnts;
+
+namespace DeadAntsTest
+{
+    [TestClass]
+    public class KataTest
+    {
+        [TestMethod]
+        public void Test_Two_Ants_Die()
+        {
+            Assert.AreEqual(2, Kata.FindOutDeadAntsCount("...ant...ant..nat.ant.t..ant...ant..ant..ant.anant..t"));
+        }
+
+        [TestMethod]
+        public void Test_No_Ants_Die()
+        {
+            Assert.AreEqual(0, Kata.FindOutDeadAntsCount(""));
+        }
+
+        [TestMethod]
+        public void Test_Three_Ants_Die()
+        {
+            Assert.AreEqual(3, Kata.FindOutDeadAntsCount("...nant..a.ant..nat.ant.t..ant...ant..ant..ant.anant..t"));
+        }
+    }
+}
