521a8a47050ce08713f2f0398ee22ff0d1a4fbfb
Briuin Lim <dreamingdexiaoxiaohao@hotmail.com>
2017-11-03 17:30:11 +0800

    add fail test

---
+++ Poker/Poker/Poker/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Poker
+{
+    public class Player
+    {
+        private readonly string _name;
+        private readonly List<string> _cards;
+
+        public Player(string name, List<string> cards)
+        {
+            _name = name;
+            _cards = cards;
+        }
+    }
+}
--- Poker/Poker/Poker/PokerGameTest.cs
+++ Poker/Poker/Poker/PokerGameTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Poker
@@ -9,6 +10,31 @@ namespace Poker
         [TestMethod]
         public void Test_BlackCard_And_WhiteCard_Are_Same_Will_Return_Tie()
         {
+            var player1 = new Player("Black", new List<string>(){"2H","3D","5S","9C","KD"});
+            var player2 = new Player("White", new List<string>(){"2H","3D","5S","9C","KD"});
+
+            var game = new Game(player1, player2);
+
+            var actual = game.GetResult();
+            var expected = "Tie.";
+            Assert.AreEqual(expected,actual);
+        }
+    }
+
+    public class Game
+    {
+        private readonly Player _player1;
+        private readonly Player _player2;
+
+        public Game(Player player1, Player player2)
+        {
+            _player1 = player1;
+            _player2 = player2;
+        }
+
+        public string GetResult()
+        {
+            throw new NotImplementedException();
         }
     }
 }
