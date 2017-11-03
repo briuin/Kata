1d53e434480049f6eabfe8668fd8a2a4baa35b50
Briuin Lim <dreamingdexiaoxiaohao@hotmail.com>
2017-11-03 17:20:15 +0800

    init project

---
+++ Poker/Poker/Poker/PokerGameTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Poker
+{
+    [TestClass]
+    public class PokerGameTest
+    {
+        [TestMethod]
+        public void Test_BlackCard_And_WhiteCard_Are_Same_Will_Return_Tie()
+        {
+        }
+    }
+}
