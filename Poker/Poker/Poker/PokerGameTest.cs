328477af5007e63190c16cef79fad1ca256bd09e
Briuin Lim <dreamingdexiaoxiaohao@hotmail.com>
2017-11-03 17:44:14 +0800

    add fail test of white high card win

--- Poker/Poker/Poker/PokerGameTest.cs
+++ Poker/Poker/Poker/PokerGameTest.cs
@@ -7,7 +7,7 @@ namespace Poker
     public class PokerGameTest
     {
         [TestMethod]
-        public void Test_BlackCard_And_WhiteCard_Are_Same_Will_Return_Tie()
+        public void Test_BlackCard_And_WhiteCard_Are_Same_Will_Output_Tie()
         {
             var player1 = new Player("Black", new List<string>(){"2H","3D","5S","9C","KD"});
             var player2 = new Player("White", new List<string>(){"2H","3D","5S","9C","KD"});
@@ -17,6 +17,19 @@ namespace Poker
             var actual = game.GetOutput();
             var expected = "Tie.";
             Assert.AreEqual(expected,actual);
+        }
+
+        [TestMethod]
+        public void Test_BlackCard_With2H_3D_5S_9C_KD_And_WhiteCard_With2H_3D_5S_9C_AD_Will_Output_White_Win()
+        {
+            var player1 = new Player("Black", new List<string>() {"2H", "3D", "5S", "9C", "KD"});
+            var player2 = new Player("White", new List<string>() {"2H", "3D", "5S", "9C", "AD"});
+
+            var game = new PokerGame(player1, player2);
+
+            var actual = game.GetOutput();
+            var expected = "White Wins. - with high card: Ace";
+            Assert.AreEqual(expected, actual);
         }
     }
 }
