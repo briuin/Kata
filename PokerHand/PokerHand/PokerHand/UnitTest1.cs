01878276e4c6f4155bb5f5283a1de3cd29abc6f3
Briuin Lim <dreamingdexiaoxiaohao@hotmail.com>
2017-11-15 20:34:46 +0800

    pass test

--- PokerHand/PokerHand/PokerHand/UnitTest1.cs
+++ PokerHand/PokerHand/PokerHand/UnitTest1.cs
@@ -14,21 +14,18 @@ namespace PokerHand
             var gameResult = game.Input(player1, player2);
             Assert.AreEqual(result, gameResult);
         }
-
-        //[TestMethod]
-        //public void Straight()
-        //{
-        //    var game = new PokerGame();
-        //    var gameResult = game.Input("2H 3H 5H 9H KH", "2C 3H 4S 8C AH");
-        //    Assert.AreEqual("White wins - with Straight",gameResult);
-        //}
     }
 
     public class PokerGame
     {
         public string Input(string black, string white)
         {
-            return "White wins - with high card: Ace";
+            var white5thCards = white.Split(' ')[4];
+            var cardValue = white5thCards[0];
+            var result = "White wins - with high card: ";
+            if (cardValue=='Q')
+                return result+"Q";
+            return result+"Ace";
         }
     }
 }
