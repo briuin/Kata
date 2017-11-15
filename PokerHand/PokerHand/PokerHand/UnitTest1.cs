cf388d6e47483d42a1ecfe272fc8f3bfb1008d70
Briuin Lim <dreamingdexiaoxiaohao@hotmail.com>
2017-11-15 20:14:35 +0800

    add testcase

--- PokerHand/PokerHand/PokerHand/UnitTest1.cs
+++ PokerHand/PokerHand/PokerHand/UnitTest1.cs
@@ -7,8 +7,19 @@ namespace PokerHand
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void HighCard()
         {
+            var game = new PokerGame();
+            var gameResult = game.Input("2H 3D 5S 9C KD", "2C 3H 4S 8C AH");
+            Assert.AreEqual("White wins - with high card: Ace", gameResult);
+        }
+    }
+
+    public class PokerGame
+    {
+        public string Input(string black, string white)
+        {
+            return "White wins - with high card: Ace";
         }
     }
 }
