ef430f464a0137ee69b557de370d07b1a3251610
Briuin Lim <dreamingdexiaoxiaohao@hotmail.com>
2017-11-15 20:36:41 +0800

    add fail test high cardQ with 4th place

--- PokerHand/PokerHand/PokerHand/UnitTest1.cs
+++ PokerHand/PokerHand/PokerHand/UnitTest1.cs
@@ -8,6 +8,7 @@ namespace PokerHand
     {
         [TestCase("2H 3D 5S 9C KD", "2C 3H 4S 8C AH", "White wins - with high card: Ace")]
         [TestCase("2H 3D 5S 9C JD", "2C 3H 4S 8C QH", "White wins - with high card: Q")]
+        [TestCase("2H 3D 5S 9C JD", "2C 3H 4S QC 8H", "White wins - with high card: Q")]
         public void HighCard(string player1, string player2, string result)
         {
             var game = new PokerGame();
@@ -23,9 +24,9 @@ namespace PokerHand
             var white5thCards = white.Split(' ')[4];
             var cardValue = white5thCards[0];
             var result = "White wins - with high card: ";
-            if (cardValue=='Q')
-                return result+"Q";
-            return result+"Ace";
+            if (cardValue == 'Q')
+                return result + "Q";
+            return result + "Ace";
         }
     }
 }
