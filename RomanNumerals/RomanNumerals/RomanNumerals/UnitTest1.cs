5cdb786d432e4ae730de67ea5665542adf5b10f3
Briuin Lim <dreamingdexiaoxiaohao@hotmail.com>
2017-11-17 17:53:43 +0800

    round 6

--- RomanNumerals/RomanNumerals/RomanNumerals/UnitTest1.cs
+++ RomanNumerals/RomanNumerals/RomanNumerals/UnitTest1.cs
@@ -9,6 +9,11 @@ namespace RomanNumerals
         [TestCase("I", 1)]
         [TestCase("V", 5)]
         [TestCase("IV", 4)]
+        [TestCase("VI", 6)]
+        [TestCase("IX", 9)]
+        [TestCase("CMXCIX", 999)]
+        [TestCase("DCXXIX", 629)]
+        [TestCase("MMMCMXCIX", 3999)]
         public void TestMethod1(string input, int expect)
         {
             var result = RomeToNumber.Translate(input);
@@ -18,23 +23,28 @@ namespace RomanNumerals
 
     public class RomeToNumber   
     {
-        static Dictionary<char, int> dict = new Dictionary<char, int>()
+        static readonly Dictionary<char, int> Dict = new Dictionary<char, int>()
         {
             {'I',1 },
-            {'V',5 }
+            {'V',5 },
+            {'X',10 },
+            {'L',50 },
+            {'C',100 },
+            {'D',500 },
+            {'M',1000 },
         };
         public static int Translate(string s)
         {
-            char[] digitArray = s.ToCharArray();
-            int result = 0;
-            for (int i = digitArray.Length - 1; i >= 0; i--)
+            var digitArray = s.ToCharArray();
+            var result = 0;
+            for (var i = digitArray.Length - 1; i >= 0; i--)
             {
-                if ( i-1 >= 0 && dict[digitArray[i - 1]] < dict[digitArray[i]])
+                if ( i != digitArray.Length - 1 && Dict[digitArray[i + 1]] > Dict[digitArray[i]])
                 {
-                    result -= dict[digitArray[i]];
+                    result -= Dict[digitArray[i]];
                     continue;
                 }
-                result += dict[digitArray[i]];
+                result += Dict[digitArray[i]];
             }
             return result;
         }
