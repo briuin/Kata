bd069d5820cad6dc58fad292693519e3f93f4836
Briuin Lim <dreamingdexiaoxiaohao@hotmail.com>
2017-11-17 17:24:20 +0800

    Round 1

--- RomanNumerals/RomanNumerals/RomanNumerals/UnitTest1.cs
+++ RomanNumerals/RomanNumerals/RomanNumerals/UnitTest1.cs
@@ -1,14 +1,31 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Collections.Generic;
+using NUnit.Framework;
 
 namespace RomanNumerals
 {
-    [TestClass]
+    [TestFixture]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        [TestCase("I", 1)]
+        [TestCase("V", 5)]
+        public void TestMethod1(string input, int expect)
         {
+            var result = RomeToNumber.Translate(input);
+            Assert.AreEqual(expect, result);
+        }
+    }
+
+    public class RomeToNumber   
+    {
+        static Dictionary<string, int> dict = new Dictionary<string, int>()
+        {
+            {"I",1 },
+            {"V",5 }
+        };
+        public static int Translate(string s)
+        {
+
+            return dict[s];
         }
     }
 }
